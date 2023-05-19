74b27a31fb9e8681b1c90f5f782d2f967884c90a
creepins <ilya.2044@mail.ru>
2023-05-19 19:25:41 +0700

    Update converter.h

--- src/libconverter/converter.h
+++ src/libconverter/converter.h
@@ -5,43 +5,43 @@
 #include <stdlib.h>
 
 /*----------------------LengthFuncs----------------------------*/
-void mm_to_sm(float mm, float rez);
-void mm_to_m(float mm, float rez);
-void mm_to_km(float mm, float rez);
-void sm_to_mm(float sm, float rez);
-void sm_to_m(float sm, float rez);
-void sm_to_km(float sm, float rez);
-void m_to_mm(float m, float rez);
-void m_to_sm(float m, float rez);
-void m_to_km(float m, float rez);
-void km_to_mm(float km, float rez);
-void km_to_sm(float km, float rez);
-void km_to_m(float km, float rez);
+float mm_to_sm(float mm, rez);
+float mm_to_m(float mm);
+float mm_to_km(float mm);
+float sm_to_mm(float sm);
+float sm_to_m(float sm);
+float sm_to_km(float sm);
+float m_to_mm(float m);
+float m_to_sm(float m);
+float m_to_km(float m);
+float km_to_mm(float km);
+float km_to_sm(float km);
+float km_to_m(float km);
 /*----------------------LengthFuncs----------------------------*/
 
 /*-----------------------TempFuncs-----------------------------*/
-void cl_to_kl(float cl, float rez);
-void cl_to_fr(float cl, float rez);
-void kl_to_cl(float kl, float rez);
-void kl_to_fr(float kl, float rez);
-void fr_to_cl(float fr, float rez);
-void fr_to_kl(float fr, float rez);
+float cl_to_kl(float cl);
+float cl_to_fr(float cl);
+float kl_to_cl(float kl);
+float kl_to_fr(float kl);
+float fr_to_cl(float fr);
+float fr_to_kl(float fr);
 /*-----------------------TempFuncs-----------------------------*/
 
 /*----------------------WeightFuncs----------------------------*/
-void gr_to_kg(float gr, float rez);
-void gr_to_tn(float gr, float rez);
-void kg_to_gr(float kg, float rez);
-void kg_to_tn(float kg, float rez);
-void tn_to_gr(float tn, float rez);
-void tn_to_kg(float tn, float rez);
+float gr_to_kg(float gr);
+float gr_to_tn(float gr);
+float kg_to_gr(float kg);
+float kg_to_tn(float kg);
+float tn_to_gr(float tn);
+float tn_to_kg(float tn);
 /*----------------------WeightFuncs----------------------------*/
 
 /*-----------------------TimeFuncs-----------------------------*/
-void sec_to_min(float sec, float rez);
-void sec_to_hour(float sec, float rez);
-void min_to_sec(float min, float rez);
-void min_to_hour(float min, float rez);
-void hour_to_sec(float hour, float rez);
-void hour_to_min(float hour, float rez);
+float sec_to_min(float sec);
+float sec_to_hour(float sec);
+float min_to_sec(float min);
+float min_to_hour(float min);
+float hour_to_sec(float hour);
+float hour_to_min(float hour);
 /*-----------------------TimeFuncs-----------------------------*/
